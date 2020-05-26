5112e3dafba5b715a6df237bd6dfb612ea40b94c
Szymon Murawski <szymon.murawski@comarch.pl>
2020-05-26 22:53:29 +0200

    DataAnnotations

---
+++ DVDRentalStore/DVDRentalStore/Migrations/202005262052292_DataAnnotations.cs
@@ -0,0 +1,24 @@
+﻿namespace DVDRentalStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotations : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.People", name: "FirstName", newName: "FName");
+            RenameColumn(table: "dbo.People", name: "LastName", newName: "LName");
+            AlterColumn("dbo.People", "FName", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.People", "LName", c => c.String(nullable: false, maxLength: 100));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.People", "LName", c => c.String());
+            AlterColumn("dbo.People", "FName", c => c.String());
+            RenameColumn(table: "dbo.People", name: "LName", newName: "LastName");
+            RenameColumn(table: "dbo.People", name: "FName", newName: "FirstName");
+        }
+    }
+}
--- DVDRentalStore/DVDRentalStore/Models/Person.cs
+++ DVDRentalStore/DVDRentalStore/Models/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,15 @@ namespace DVDRentalStore.Models
     public abstract class Person
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Column("FName")]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [Column("LName")]
         public string LastName { get; set; }
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
     }
 }
